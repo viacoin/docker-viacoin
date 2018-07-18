7af69640d2e32f6a1449ae3107563a26db10b416
Romano <romanornr@gmail.com>
2018-07-19 00:40:05 +0200

    Viacoin datadir fix try

--- viacoin/0.15.1/docker-entrypoint.sh
+++ viacoin/0.15.1/docker-entrypoint.sh
@@ -2,23 +2,23 @@
 set -e
 
 if [[ "$1" == "viacoin-cli" || "$1" == "viacoin-tx" || "$1" == "viacoind" || "$1" == "test_viacoin" ]]; then
-	mkdir -p "$VIACOIN_DATA"
+	mkdir -p "$BITCOIN_DATA"
 
-	cat <<-EOF > "$VIACOIN_DATA/viacoin.conf"
+	cat <<-EOF > "$BITCOIN_DATA/viacoin.conf"
 	printtoconsole=1
 	rpcallowip=::/0
-	${VIACOIN_EXTRA_ARGS}
+	${BITCOIN_EXTRA_ARGS}
 	EOF
-	chown viacoin:viacoin "$VIACOIN_DATA/viacoin.conf"
+	chown bitcoin:bitcoin "$BITCOIN_DATA/viacoin.conf"
 
 	# ensure correct ownership and linking of data directory
 	# we do not update group ownership here, in case users want to mount
 	# a host directory and still retain access to it
-	chown -R viacoin "$VIACOIN_DATA"
-	ln -sfn "$VIACOIN_DATA" /home/viacoin/.viacoin
-	chown -h viacoin:viacoin /home/viacoin/.viacoin
+	chown -R bitcoin "$BITCOIN_DATA"
+	ln -sfn "$BITCOIN_DATA" /home/bitcoin/.viacoin
+	chown -h bitcoin:bitcoin /home/bitcoin/.viacoin
 
-	exec gosu viacoin "$@"
+	exec gosu bitcoin "$@"
 else
 	exec "$@"
 fi
